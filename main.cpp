4fe4becf48800824a9d58960e9340945dfe6681b
Paxton Schweigert <paxmanrocks@gmail.com>
2022-03-20 12:08:47 -0700

    add various compatibility changes so that this project is easily compiled with visual studio.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ extern "C" {
 #include "memory.h"
 }
 
-static u_int8_t *getMemory(uint16_t address) {
+static uint8_t *getMemory(uint16_t address) {
     static auto *mem_arr = new uint8_t[1 << 16];
 
     return &mem_arr[address];
@@ -19,15 +19,15 @@ static void threadSleep(double sec) {
 int main() {
     initMemory(getMemory);
 
-    loadBinFile("6510_Emulator/testing/nestest.nes", 0x8000, 0x0010, 0x4000);
-    loadBinFile("6510_Emulator/testing/nestest.nes", 0xC000, 0x0010, 0x4000);
+    loadBinFile(PROJECT_ROOT"/6510_Emulator/testing/nestest.nes", 0x8000, 0x0010, 0x4000);
+    loadBinFile(PROJECT_ROOT"/6510_Emulator/testing/nestest.nes", 0xC000, 0x0010, 0x4000);
 
-    u_int8_t *mem = getMemoryPtr(INT_RESET+1);
+    uint8_t *mem = getMemoryPtr(INT_RESET+1);
     *mem = 0xC0;
     *(mem - 1) = 0x00;
 
     setSleepFunction(threadSleep);
-    setCPUFreq(1000);
+    //setCPUFreq(1000000);
     initCPU();
     std::thread cpu_thread{runLoop, stdout};
     cpu_thread.join();
