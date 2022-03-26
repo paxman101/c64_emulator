e6816c467ba5dd715fd8e11006c6ff0d6bd69a6c
Paxton Schweigert <paxmanrocks@gmail.com>
2022-03-25 20:00:32 -0700

    start implementing run for Vic

--- vic.cpp
+++ vic.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "vic.h"
+#include "memory.h"
 
 void Vic::setRegister(uint_fast16_t reg_offset, uint8_t val) {
     // Only the lower 6 address lines are read for registers
@@ -11,6 +12,15 @@ void Vic::setRegister(uint_fast16_t reg_offset, uint8_t val) {
     // Don't change fake registers
     if (reg_index >= 47) {
         return;
+    }
+
+    // Writes to RST set the interrupt line
+    if (reg_index == RST) {
+        raster_int_ = val;
+        return;
+    }
+    if (reg_index == CTRL1) {
+        raster_int_ &= val >> 7;
     }
 
     registers[reg_index] = val;
@@ -41,4 +51,30 @@ uint8_t Vic::getRegister(uint_fast16_t reg_offset) {
     }
 
     return registers[reg_index];
+}
+
+void Vic::run() {
+    static unsigned int cycle_count = 0;
+    cycle_count++;
+
+    // bit 3 of control registers refer to rsel and csel respectively
+    bool rsel = (registers[CTRL1] & 0x08) >> 3;
+    bool csel = (registers[CTRL2] & 0x08) >> 3;
+
+    // raster line is defined by the raster register and bit 7 of the control 1 register
+    unsigned int raster = (registers[CTRL1] >> 7) * 0x100 + registers[RST];
+
+    // increment raster if done
+    if (cycle_count == cycles_per_line) {
+        cycle_count = 0;
+        raster++;
+
+        // wrap around at end of lines.
+        if (raster > num_lines) {
+            raster = 0;
+        }
+
+        registers[RST] = raster & 0xFF;
+        registers[CTRL1] = ((raster & 0x100) >> 1) | (registers[CTRL1] & 0x7F);
+    }
 }
